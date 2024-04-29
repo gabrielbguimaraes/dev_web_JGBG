5c70363ad36f032df50c0a5b7e3133bce289c65c
Gabriel Guimarães <biel_google@hotmail.com>
2024-04-29 17:14:26 -0300

    reworkando tudo

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/contatos')
-def contatos():
-    return render_template('contatos.html')
-
-@app.route('/quemsomos')
-def quem_somos():
-    return render_template('quemsomos.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
