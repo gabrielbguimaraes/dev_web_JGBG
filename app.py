829b4d0e664c3899afecac95de5fd3dbce80f4e1
Gabriel Guimarães <biel_google@hotmail.com>
2024-04-29 21:54:00 -0300

    Subindo o desafio_3 da materia de dev-web

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/video')
+def video():
+    return render_template('video.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
