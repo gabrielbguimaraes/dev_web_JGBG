6be53048accb7f434f9aef0cbff88a0cc291a0b5
Gabriel <biel_google@hotmail.com>
2024-05-21 11:33:35 -0300

    Finalizando a aplicação

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
-from dotenv import load_dotenv
 
 app = Flask(__name__, static_url_path='/static')
 
+conexao = mysql.connector.connect(
+    host="mysql",
+    user="root",
+    password="#Ovelha354",
+    database="registro_onepiece"
+)
+cursor = conexao.cursor()
 
 @app.route('/')
 def index():
@@ -17,16 +23,16 @@ def video():
 def manga():
     return render_template('/manga.html')
 
-conexao = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="#Ovelha354",
-    database="registro_onepiece"
-)
-cursor = conexao.cursor()
-
 @app.route('/cadastro.html', methods=['GET','POST'])
 def cadastro():
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT COUNT(*) FROM users_opb")
+    count = cursor.fetchone()["COUNT(*)"]
+
+    if count >= 15:
+        cursor.execute("DELETE FROM users_opb")
+        conexao.commit()
+
     if request.method == 'POST':
         nome = request.form["username"]
         email = request.form["email"]
@@ -37,17 +43,22 @@ def cadastro():
 
         # Consulta SQL para inserir um novo registro na tabela
         consulta = "INSERT INTO users_opb (nome_usuario, email, senha) VALUES (%s, %s, %s)"
-
         valores = (nome, email, senha)
 
         cursor.execute(consulta, valores)
-
         conexao.commit()
 
         cursor.close()
-        return render_template('/cadastro.html')
+        return redirect(url_for('lista_usuarios'))  # Redirecionar para a página de usuários após o cadastro
     else:
         return render_template('/cadastro.html')
+
+@app.route('/users.html')
+def lista_usuarios():
+    cursor = conexao.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users_opb")
+    users = cursor.fetchall()
+    return render_template('/users.html', users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
