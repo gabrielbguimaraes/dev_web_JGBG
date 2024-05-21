1ff2169c8630910615d12d7c7afa255ae911b4f2
Gabriel Guimarães <41351830+gabrielbguimaraes@users.noreply.github.com>
2024-05-21 18:36:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def lista_usuarios():
     return render_template('/users.html', users=users)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=5000)
